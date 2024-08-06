08609603b8a9dfd26d920c52921d2edc8834c63b
Jeremydfrancis <jeremydfrancis0@gmail.com>
2024-08-06 15:26:34 -0400

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import HeaderBox from '@/components/HeaderBox';
+import RightSideBar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react';
 
 const Home = () => {
-  return <div>Home Dashboard</div>;
+  const loggedIn = { firstName: 'Jeremy', lastName: 'Francis', email: 'jeremydfrancis0@gmail.com' };
+  return <section className='home'>
+    <div className='home-content'>
+      <header className='home-header'>
+        <HeaderBox
+        type='greeting'
+        title='Welcome'
+        user={loggedIn?.firstName || 'Guest'}
+        subtext='Access and manage your account with ease.'
+
+        />
+        <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+      </header>
+      RECENT TRANSACTIONS
+    </div>
+    <RightSideBar
+    user={loggedIn}
+    transactions={[]}
+    banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+
+    />   
+  </section>;
 };
 
 export default Home;
