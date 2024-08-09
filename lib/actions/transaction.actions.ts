aaa0c0823db7eead38206ee92e0884adb53368a8
Jeremydfrancis <jeremydfrancis0@gmail.com>
2024-08-08 23:06:11 -0400

    fixed transaction list not showing examples

--- lib/actions/transaction.actions.ts
+++ lib/actions/transaction.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ID, Query } from "node-appwrite";
+import { ID } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { parseStringify } from "../utils";
 
@@ -9,7 +9,9 @@ const {
   APPWRITE_TRANSACTION_COLLECTION_ID: TRANSACTION_COLLECTION_ID,
 } = process.env;
 
-export const createTransaction = async (transaction: CreateTransactionProps) => {
+export const createTransaction = async (
+  transaction: CreateTransactionProps
+) => {
   try {
     const { database } = await createAdminClient();
 
@@ -18,44 +20,49 @@ export const createTransaction = async (transaction: CreateTransactionProps) => 
       TRANSACTION_COLLECTION_ID!,
       ID.unique(),
       {
-        channel: 'online',
-        category: 'Transfer',
-        ...transaction
+        channel: "online",
+        category: "Transfer",
+        ...transaction,
       }
-    )
+    );
 
     return parseStringify(newTransaction);
   } catch (error) {
-    console.log(error);
+    console.error(error)
   }
-}
+};
 
-export const getTransactionsByBankId = async ({bankId}: getTransactionsByBankIdProps) => {
+export const getTransactionsByBankId = async ({
+  bankId,
+}: getTransactionsByBankIdProps) => {
   try {
     const { database } = await createAdminClient();
 
     const senderTransactions = await database.listDocuments(
       DATABASE_ID!,
-      TRANSACTION_COLLECTION_ID!,
-      [Query.equal('senderBankId', bankId)],
-    )
+      TRANSACTION_COLLECTION_ID!
+      // [Query.equal("senderBankId", bankId)]
+    );
+
+    
 
     const receiverTransactions = await database.listDocuments(
       DATABASE_ID!,
-      TRANSACTION_COLLECTION_ID!,
-      [Query.equal('receiverBankId', bankId)],
+      TRANSACTION_COLLECTION_ID!
+      // [Query.equal("receiverBankId", bankId)]
     );
+
 
     const transactions = {
       total: senderTransactions.total + receiverTransactions.total,
       documents: [
-        ...senderTransactions.documents, 
+        ...senderTransactions.documents,
         ...receiverTransactions.documents,
-      ]
-    }
+      ],
+    };
 
     return parseStringify(transactions);
   } catch (error) {
-    console.log(error);
+    console.error(error)
   }
-}
+};
